413d283cf32773771cbe72ad59f25cacb3d96cbc
emmanuel <emmanuel.roberson3@gmail.com>
2022-04-14 18:19:42 -0500

    Instead of set rotations, use simple continuous rotation

--- SpaceOddity/Assets/Scripting/Monobehaviours/Camera Behavior/PlayerCameraRotateBehaviour.cs
+++ SpaceOddity/Assets/Scripting/Monobehaviours/Camera Behavior/PlayerCameraRotateBehaviour.cs
@@ -14,20 +14,7 @@ namespace Camera
         // Transform for this gameobject
         private Transform _selfTransform;
 
-        // Target value for y rotation to reach
-        private Quaternion _rotationTarget;
-        private Quaternion _previousRotation;
-
-        // Acts as a check to prevent rotation spamming,
-        // and spamming could lead to bugs.
-        private bool rotationComplete;
-
         public float RotationSpeed;
-
-        public float timeCount;
-
-        // Rotation velocity reference (used in Vector3.SmoothDamp)
-        private Vector3 _rotationVelocity;
 
         // Start is called before the first frame update
         void Start()
@@ -37,27 +24,54 @@ namespace Camera
 
             // Get transform reference
             _selfTransform = transform;
-
-            rotationComplete = true;
-
-            timeCount = 0;
         }
 
         // Update is called once per frame
         void Update()
         {
 
-            _selfTransform.rotation = Quaternion.Slerp(_previousRotation, _rotationTarget, timeCount);
-
-            timeCount += Time.deltaTime;
-
         }
 
-        public void Rotate(float rotationAmount)
+        public void RotateLeft()
         {
+            _selfTransform.Rotate(_selfTransform.up, Time.deltaTime * RotationSpeed * -1);
+        }
 
-            _previousRotation = transform.rotation;
+        public void RotateRight()
+        {
+            _selfTransform.Rotate(_selfTransform.up, Time.deltaTime * RotationSpeed * 1);
+        }
 
+        IEnumerator RotationCoroutine(float rotationAmount)
+        {
+            /*
+            float startingRotation = _selfTransform.rotation.y;
+            float rotationTarget = _selfTransform.rotation.y + rotationAmount;
+            float rotationVelocity = 0;
+            float amountRotated = 0;
+
+            while (Math.Abs(amountRotated) <= Math.Abs(rotationAmount))
+            {
+
+                var currentRotationStep = Mathf.SmoothStep(startingRotation, rotationTarget, Time.deltaTime * RotationSpeed);
+
+                //var currentRotationStep =
+                 //   Mathf.SmoothDamp(_selfTransform.rotation.y, rotationTarget, ref rotationVelocity, 0.2f);
+
+                _selfTransform.Rotate(_selfTransform.up, currentRotationStep);
+
+                amountRotated += Math.Abs(currentRotationStep);
+
+                yield return 0;
+            }
+
+            if (Math.Abs(360 - _selfTransform.rotation.y) < rotationAmount/2)
+            {
+                _selfTransform.SetPositionAndRotation(transform.position, new Quaternion(_selfTransform.rotation.x, 0, _selfTransform.rotation.z, _selfTransform.rotation.w));
+            }
+            */
+
+            yield return 0;
         }
     }
 }
